ac9fcda6b42a8d2c1a1f97e8980463e3bd72892b
SnehaMishra05 <139227421+SnehaMishra05@users.noreply.github.com>
2025-03-12 09:12:54 +0530

    Update dp.py

--- algorithms/dp.py
+++ algorithms/dp.py
@@ -34,7 +34,7 @@ def mcm(arr):
         dp[i][i] = 0
 
     for l in range(2, n):
-        for i in range(1, n - l + 1):
+        for i in range(1, n - l + 2):
             j = i + l - 1
             for k in range(i, j):
                 cost = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j]
