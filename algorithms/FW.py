f2fe72ae32f0df2240e819f93928f910694a81e9
Shubhangini <shubhangini.p.sarathy@gmail.com>
2025-03-28 01:10:51 +0530

    corrections

--- algorithms/FW.py
+++ algorithms/FW.py
@@ -1,16 +1,20 @@
 def floydwarshall(n, G):
+    # Create a copy of the input matrix
     distance = [row[:] for row in G]
-
-
+    
+    # Floyd-Warshall algorithm
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
-
+                if distance[i][k] != float('inf') and distance[k][j] != float('inf'):
+                    distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    
+    # Replace infinity values with '∞' for display
     for i in range(n):
         for j in range(n):
-            if (distance[i][j] >= 999):
+            if distance[i][j] == float('inf'):
                 distance[i][j] = '∞'
-               
-
+            elif distance[i][j] >= 999:  # Handle large numbers that represent infinity
+                distance[i][j] = '∞'
+    
     return distance
