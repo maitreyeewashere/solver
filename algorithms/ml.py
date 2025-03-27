afb3068d757402383551a517c54b4135e58ba7f9
maitreyee <48650686+maitreyeewashere@users.noreply.github.com>
2025-03-28 00:48:46 +0530

    Update ml.py

--- algorithms/ml.py
+++ algorithms/ml.py
@@ -2,19 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Linear Regression
+
 def linear_regression():
-    # Sample Data
-    X = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).reshape(-1, 1)
     y = np.array([3, 4, 2, 5, 6, 8, 9, 10, 12, 11])
 
     model = LinearRegression()
     model.fit(X, y)
 
-    # Predictions
     y_pred = model.predict(X)
 
-    # Plotting
     plt.scatter(X, y, color='blue', label='Actual Data')
     plt.plot(X, y_pred, color='red', linewidth=2, label='Regression Line')
 
