655fa3e4197c03fb468b884e66d2577cb043afde
maitreyee <48650686+maitreyeewashere@users.noreply.github.com>
2025-03-28 01:05:06 +0530

    FW rendering

--- algorithms/FW.py
+++ algorithms/FW.py
@@ -1,16 +1,18 @@
 def floydwarshall(n, G):
     distance = [row[:] for row in G]
-
+    
 
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+                if distance[i][k] != float('inf') and distance[k][j] != float('inf'):
+                    distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
 
     for i in range(n):
         for j in range(n):
-            if (distance[i][j] >= 999):
+            if distance[i][j] == float('inf'):
                 distance[i][j] = '∞'
-               
-
+            elif distance[i][j] >= 999:
+                distance[i][j] = '∞'
+    
     return distance
